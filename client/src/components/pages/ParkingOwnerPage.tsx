0dcfdea6b51363e2b8437c82e3399dc33c485a48
Gadyuka0514 <NikitaRF09@rambler.ru>
2024-11-14 12:17:50 +0300

    rel

--- client/src/components/pages/ParkingOwnerPage.tsx
+++ client/src/components/pages/ParkingOwnerPage.tsx
@@ -128,7 +128,7 @@ export default function ParkingOwnerPage() {
             size="small"
             onClick={handleOpen}
           >
-            Добавить парковочные места
+            Добавить новую парковку          
           </Button>
           <Modal
             open={isModalOpen}
